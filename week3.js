75b2bf5e639b34644a9f115e7033fb36c61a1650
MD.Malik <119576349+minomalik@users.noreply.github.com>
2023-03-24 00:55:40 +0545

    Add files via upload

---
+++ week3.js
@@ -0,0 +1,189 @@
+// function triArea(b,h){
+//     area = b * h * 1/2;
+//     return area;
+// }
+// t1=triArea(3,2);
+// t2=triArea(7,4);
+// t3=triArea(10,10);
+// console.log("Area:",t1);
+// console.log("Area:",t2);
+// console.log("Area:",t3);
+
+
+
+
+
+
+
+// function giveMeSomething(a){
+//     return("something"+" "+a);
+// }
+// console.log(giveMeSomething("is better than nothing"));
+// console.log(giveMeSomething("Bob Jane"));
+// console.log(giveMeSomething("something"));
+
+
+
+
+
+
+// function points(p1,p2) {
+//     let g = p1*2;
+//     let h = p2*3;
+//     let total = g + h;
+//     return total;
+// }
+// let a = points(1,1);
+// let b = points(7,5);
+// let c = points(38,8);
+// console.log("Point1: ",a);
+// console.log("Point: ",b);
+// console.log("Point: ",c);
+
+
+
+
+
+
+
+// function lessThan100(a,b) {
+//     let c = a + b;
+//     if (c<100){
+//         return true;
+//     }
+//     return false;
+// }
+// let case1 = lessThan100(22,15);
+// let case2 = lessThan100(83,34);
+// let case3 = lessThan100(3,77); 
+// console.log("Case1: ",case1);
+// console.log("Case2: ",case2);
+// console.log("case3: ",case3);
+
+
+
+
+
+
+
+// function addUp(n) {
+//     let s = 0;
+//     for (let i = 0; i<=n; i++){
+//         s = s + i
+//     }
+//     return s;
+//     }
+//     n1 = addUp(4);
+//     n2 = addUp(13);
+//     n3 = addUp(600);
+//     console.log("Number1: ",n1);
+//     console.log("Number2: ",n2);
+//     console.log("Number3: ",n3);
+
+
+
+
+
+
+
+    // function oddishOrEvenish (a) {
+    //     let s = 0;
+    //     while (a >0){
+    //         c = a % 10;
+    //         s += c;
+    //         a = (a - c)/10;
+    //     }
+    //     if (s%2==0){
+    //         return "Evenish";
+    //     }
+    //     else {
+    //         return "Oddish";
+    //     }
+    //     }
+        
+    //     x = oddishOrEvenish(43);
+    //     y = oddishOrEvenish(373);
+    //     z = oddishOrEvenish(4433);
+    //     console.log("num1: ",x);
+    //     console.log("num2: ",y);
+    //     console.log("num3: ",z);
+
+
+
+
+
+
+
+        // function primeInRange(n1,n2){
+        //     for (let i=n1; i<=n2; i++){
+        //       let isPrime = true;
+        //       if (i<=1){
+        //         isPrime = false;
+        //       }
+        //       else {
+        //         for (let j=2; j <= Math.sqrt(i); j++){
+        //           if (i %j ===0){
+        //             isPrime = false;
+        //             break;
+        //           }
+        //         }
+        //       }
+        //       if(isPrime){
+        //         console.log(i);
+        //         return true;
+        //       }
+        //     }
+        //     return false;
+        //     }
+        //     let pri = primeInRange(10,20);
+        //     console.log(pri);
+
+
+
+
+
+
+
+
+            // function shiftToLeft(a,b) {
+            //     c = a<<b;
+            //     return c;
+            // }
+            // i = shiftToLeft(5,2);
+            // j = shiftToLeft(10,3);
+            // k = shiftToLeft(-32,2);
+            // l = shiftToLeft(-6,5);
+            // m = shiftToLeft(12,4);
+            // n = shiftToLeft(46,6);
+            // console.log("Q1: ",i);
+            // console.log("Q2: ",j);
+            // console.log("Q3: ",k);
+            // console.log("Q4: ",l);
+            // console.log("Q5: ",m);
+            // console.log("Q6: ",n);
+
+
+
+
+            
+
+
+
+
+
+            // function binary(no){
+            //     let num = '';
+            //     let b = no;
+            //     while (b !==0){
+            //         num = (b%2) + num;
+            //         b = Math.floor(b/2);
+            //     }
+            //     return num;
+            // }
+            // let q1 = binary(1);
+            // let q2 = binary(5);
+            // let q3 = binary(10);
+            // console.log("binary of 1: ",q1);
+            // console.log("binary of 5: ",q2);
+            // console.log("binary of 10: ",q3);
+            
